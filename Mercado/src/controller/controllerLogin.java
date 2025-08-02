594230001c531aee54ce2ee21c12f34a9c2b3324
sistema.noturno <sistema.noturno@LAB-03-29.sistemafieto.rede>
2025-08-01 21:55:31 -0300

    Implementação de Telas

---
+++ Mercado/src/controller/controllerLogin.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class controllerLogin {
+
+    @FXML
+    private Button btLogin;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    void actionLogin(ActionEvent event) {
+
+    }
+
+}
